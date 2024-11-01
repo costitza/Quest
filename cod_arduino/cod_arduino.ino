f498414adc5d495be5aaf25890929c868eaf353c
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2024-11-01 20:03:37 +0200

    arduino code rc

--- cod_arduino/cod_arduino.ino
+++ cod_arduino/cod_arduino.ino
@@ -14,30 +14,42 @@ const int BACK_TRIG_PIN = 15;
 const int BACK_ECHO_PIN = 17;
 const int MAX_DISTANCE = 200;
 
+// Create NewPing instances for each sensor
 NewPing front_sensor(FRONT_TRIG_PIN, FRONT_ECHO_PIN, MAX_DISTANCE);
 NewPing right_sensor(RIGHT_TRIG_PIN, RIGHT_ECHO_PIN, MAX_DISTANCE);
 NewPing left_sensor(LEFT_TRIG_PIN, LEFT_ECHO_PIN, MAX_DISTANCE);
 NewPing back_sensor(BACK_TRIG_PIN, BACK_ECHO_PIN, MAX_DISTANCE);
 
-const int DHT_PIN = 31; 
+// Define pin for DHT sensor
+const int DHT_PIN = 31;  // GPIO pin connected to DHT11 sensor
 
+// Initialize DHT sensor
 DHT dht(DHT_PIN, DHT11);
 
 // Motor pin definitions
-const int motorPins[4][2] = {{10, 8}, {5, 7}, {6, 14}, {11, 9}}; 
+const int motorPins[4][2] = {{8, 10}, {14, 6}, {5, 7}, {9, 11}}; // Pins for four motors
 //                    
-const int motorEnPins[4] = {13, 12, 2, 3};
+const int motorEnPins[4] = {13, 12, 2, 3}; // Enable pin for the motors
 
 // Servo pin definitions
 const int switchServosPins[4] = {47, 44, 42, 40};  // Pins for the drone mode servos
 const int legServosPins[4] = {38, 36, 34, 32};    // Pins for the leg movement servos
 
 int currentAngles[4] = {0, 0, 0, 0};
+// angle-uri pentru drive/drone mode la switch servo
+int anglesDriveSwitch[4] = {20, 161, 71, 163};
+int anglesDroneSwitch[4] = {119, 65, 169, 64};
+
+// angle-uri pentru normal/streched la leg ervo
+int anglesNormalLeg[4] = {151, 82, 83, 106};
+int anglesStrechedLeg[4] = {71, 160, 3, 180};
+int anglesNormalDroneLeg[4] = {151 , 85, 83, 108};
 
 Servo switchServos[4];
 Servo legServos[4];
 
 void setup() {
+    // Set up motor pins
     for (int i = 0; i < 4; i++) {
         pinMode(motorPins[i][0], OUTPUT);
         pinMode(motorPins[i][1], OUTPUT);
@@ -57,16 +69,24 @@ void setup() {
         legServos[i].attach(legServosPins[i]);
     }
 
+    for(int i=0; i < 4; i++)
+    {
+      switchServos[i].write(anglesDriveSwitch[i]);
+    }
+
+    for(int i = 0;i < 4; i++){
+      legServos[i].write(anglesNormalLeg[i]);
+    }
+
     // Initialize DHT sensor
     dht.begin();
     Serial.println("DHT sensor initialized.");
 
-    Serial.begin(115200); // Match the baud rate used in Raspberry Pi code
+    Serial.begin(115200);
     Serial.println("Arduino is ready to receive commands");
 }
 
 void driveWheels(int direction, int power) {
-    // direction: 0=Stop, 1=Forward, 2=Backward, 3=Counterclockwise, 4=Clockwise
     switch(power) {
         case 4:
             for (int i = 0; i < 4; i++) {
@@ -170,12 +190,22 @@ void driveWheels(int direction, int power) {
 
 void Drive(){
     int currentAnglesSwitch[4] = {switchServos[0].read(), switchServos[1].read(), switchServos[2].read(), switchServos[3].read()};
-    int anglesSwitch[4] = {8, 155, 80, 170};
+    int anglesSwitch[4];
     int anglesSwitchReached[4] = {0};
-    if(currentAnglesSwitch[0] != anglesSwitch[0]){
+    
+    for (int i = 0; i < 4; i++) {
+        anglesSwitch[i] = anglesDriveSwitch[i];
+    }
+
+    if(currentAnglesSwitch[0] != anglesSwitch[0]) {
       int currentAnglesLegs[4] = {legServos[0].read(), legServos[1].read(), legServos[2].read(), legServos[3].read()};
-      int anglesLegs[4] = {33, 160, 65, 125};
+      int anglesLegs[4];
       int anglesLegsReached[4] = {0};
+
+      for (int i = 0; i < 4; i++) {
+          anglesLegs[i] = anglesStrechedLeg[i];
+      }
+
       while (anglesSwitchReached[0] + anglesSwitchReached[1] + anglesSwitchReached[2] + anglesSwitchReached[3] != 4 ||
       anglesLegsReached[0] + anglesLegsReached[1] + anglesLegsReached[2] + anglesLegsReached[3] != 4) {
           for (int i = 0; i < 4; i++) {
@@ -202,14 +232,18 @@ void Drive(){
       driveWheels(6, 0);
       adjustLegsNormal();
       driveWheels(0, -1);
-
     }
 }
 
 void Drone(){
     int currentAnglesSwitch[4] = {switchServos[0].read(), switchServos[1].read(), switchServos[2].read(), switchServos[3].read()};
-    int anglesSwitch[4] = {110, 50, 180, 60};
+    int anglesSwitch[4];
     int anglesSwitchReached[4] = {0};
+
+    for (int i = 0; i < 4; i++) {
+        anglesSwitch[i] = anglesDroneSwitch[i];
+    }
+
     if(currentAnglesSwitch[0] != anglesSwitch[0]){
       driveWheels(5, 0);
       adjustLegsStretched();
@@ -217,8 +251,13 @@ void Drone(){
       delay(300);
 
       int currentAnglesLegs[4] = {legServos[0].read(), legServos[1].read(), legServos[2].read(), legServos[3].read()};
-      int anglesLegs[4] = {110, 85, 135, 60};
+      int anglesLegs[4]; // Declare the local array
       int anglesLegsReached[4] = {0};
+
+      for (int i = 0; i < 4; i++) {
+          anglesLegs[i] = anglesNormalDroneLeg[i];
+      }
+
       while (anglesSwitchReached[0] + anglesSwitchReached[1] + anglesSwitchReached[2] + anglesSwitchReached[3] != 4 ||
       anglesLegsReached[0] + anglesLegsReached[1] + anglesLegsReached[2] + anglesLegsReached[3] != 4) {
           for (int i = 0; i < 4; i++) {
@@ -229,6 +268,7 @@ void Drone(){
               } else {
                   anglesSwitchReached[i] = 1;
               }
+
               if (currentAnglesLegs[i] != anglesLegs[i]) {
                   int increment = (currentAnglesLegs[i] < anglesLegs[i]) ? 1 : -1;
                   currentAnglesLegs[i] += increment;
@@ -244,8 +284,13 @@ void Drone(){
 
 void adjustLegsNormal() {
     int currentAngles[4] = {legServos[0].read(), legServos[1].read(), legServos[2].read(), legServos[3].read()};
-    int angles[4] = {103, 90, 130, 60};
+    int angles[4]; // Declare the local array
     int anglesReached[4] = {0};
+
+    // Copy values from global anglesNormalLeg array
+    for (int i = 0; i < 4; i++) {
+        angles[i] = anglesNormalLeg[i];
+    }
 
     while (anglesReached[0] + anglesReached[1] + anglesReached[2] + anglesReached[3] != 4) {
         for (int i = 0; i < 4; i++) {
@@ -263,8 +308,13 @@ void adjustLegsNormal() {
 
 void adjustLegsStretched() {
     int currentAngles[4] = {legServos[0].read(), legServos[1].read(), legServos[2].read(), legServos[3].read()};
-    int angles[4] = {33, 160, 65, 125};
+    int angles[4]; // Declare the local array
     int anglesReached[4] = {0};
+
+    // Copy values from global anglesStrechedLeg array
+    for (int i = 0; i < 4; i++) {
+        angles[i] = anglesStrechedLeg[i];
+    }
 
     while (anglesReached[0] + anglesReached[1] + anglesReached[2] + anglesReached[3] != 4) {
         for (int i = 0; i < 4; i++) {
@@ -280,13 +330,8 @@ void adjustLegsStretched() {
     }
 }
 
-bool ok = true;
 void loop() {
-  if(ok==true)
-  {
-    Drive();
-    ok= false;
-  }
+    // Read distances from ultrasonic sensors
   int front_distance = front_sensor.ping_cm();
   int right_distance = right_sensor.ping_cm();
   int left_distance = left_sensor.ping_cm();
@@ -307,18 +352,18 @@ void loop() {
   Serial.print(",");
   Serial.println(humidity);
   if (Serial.available() > 0) {
-        String incomingString = Serial.readStringUntil('\n');
+      String incomingString = Serial.readStringUntil('\n');
+      char mode = incomingString[0];
+      if (mode == '0') {
+          // Fly command
+          Drone();
+      } else if (mode == '1') {
+          int direction = incomingString[1] - '0';
+          int power = incomingString[2] - '0';
+          // Drive command
+          Drive();
 
-        char mode = incomingString[0];
-
-        if (mode == '0') {
-            Drone();
-        } else if (mode == '1') {
-            int direction = incomingString[1] - '0';
-            int power = incomingString[2] - '0';
-            Drive();
-
-            driveWheels(direction, power);
-        }
-    }
+          driveWheels(direction, power);
+      }
+  }
 }
